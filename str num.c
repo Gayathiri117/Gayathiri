5df50012eaeaf4f5f59785d117887ba1bd106ec5
Gayathiri117 <gayathiri.it13@bitsathy.ac.in>
2016-06-08 09:23:00 +0530

    Create str num.c

---
+++ str num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char str[100];
+    int i;
+    printf("\n Enter the string");
+    scanf("%s",str);
+    i=atoi(str);
+    printf("\n The number is",i);
+    return 0;
+}
